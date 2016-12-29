b05c898556b800ec0ade68d1f40c42127446b367
John Hancock <jhancock50124@gmail.com>
2016-12-29 16:08:33 -0500

    Modifications to core

--- openbot/core.py
+++ openbot/core.py
@@ -1,10 +1,29 @@
+import os
+import asyncio
+
+from openbot.permissions import BotPerms
 from openbot.config import ConfigStream
 
 class BotCore(config_file='config/openbot.json'):
+
   def __init__(self):
-    config = ConfigStream()
-    plugins = self.load_plugins()
+    self.config = ConfigStream()
+    self.server = None
+    self.permissions = BotPerms()
+
+    self.plugins = self.load_plugins()
+    self.tasks = self.load_tasks()
+    self.functions = self.load_functions()
+
+  async def load_plugins(self):
+
     pass
 
-  def load_plugins(self):
+  async def load_tasks(self):
     pass
+
+  async def load_functions(self):
+    pass
+
+  async def fuck_you_bitch(self):
+    return "No, fuck you"
