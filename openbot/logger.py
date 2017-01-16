226172491b610932c04f4e27fc443770e0ef593b
John Hancock <jhancock50124+gh@gmail.com>
2017-01-15 20:53:19 -0500

    Added logger

--- openbot/logger.py
+++ openbot/logger.py
@@ -29,7 +29,7 @@ class Logger:
 
   def _load_locale(self, locale):
     try:
-      with open('../locale/{}.json'.format(locale), "r") as file:
+      with open('locale/{}.json'.format(locale), "r") as file:
         return json.loads(file.read())
     except FileNotFoundError:
       if locale != 'en_us':
@@ -40,13 +40,38 @@ class Logger:
         exit(66)
 
 
-  def log(self, message, parent='core.info.plaintext', type=LogLevel.info, error_point=None):
-    parent_string = self._get_locale_string(parent)\
-      .format(message=message, error_point=error_point)
-    base_string = self._get_locale_string("base.{}_base".format(type.name))\
-      .format(message=parent_string, **self.locale['colors'], **self.locale['format'])
+  def log(self, message, parent='core.info.plaintext', type=LogLevel.info, error_point=None, send_to_chat=True):
+    try:
+      parent_string = self.get_locale_string(parent)\
+        .format(message=message, error_point=error_point)
+      cli_base_string = self.get_locale_string("base.cli.{}_base".format(type.name))\
+        .format(message=parent_string, **self.locale['colors'], **self.locale['format'])
+      # TODO: Fix some fuckery here
 
-    self._print(base_string)
+      self._print(cli_base_string)
+
+      if send_to_chat:
+        chat_base_string = self.get_locale_string("base.chat.{}_base".format(type.name))\
+          .format(message=parent_string)
+        # TODO: Handle chat messages
+        pass
+
+    except ParentNotFoundException:
+      # TODO: Fix this shit, don't to this ever -jhnhnck
+      self.log("\"{}\" with level \"{}\"".format(message, type.name),
+               parent="core.error.locale_missing",
+               type=LogLevel.error,
+               error_point=parent,
+               send_to_chat=send_to_chat)
+    except ParentScopeException:
+      # TODO: Fix this shit, don't to this ever -jhnhnck
+      self.log("\"{}\" with level \"{}\"".format(message, type.name),
+               parent="core.error.locale_early_termination",
+               type=LogLevel.error,
+               error_point=parent,
+               send_to_chat=send_to_chat)
+    except IndexError:
+      self.log()
 
 
   @staticmethod
@@ -54,7 +79,7 @@ class Logger:
     sys.stdout.write(message + '\n')
 
 
-  def _get_locale_string(self, parent):
+  def get_locale_string(self, parent):
     store = self.locale
 
     try:
@@ -62,7 +87,17 @@ class Logger:
         store = store[branch]
       if not isinstance(store, str):
         raise ParentScopeException
-    except IndexError:
+    except KeyError:
       raise ParentNotFoundException
 
     return store
+
+
+  def self_test(self):
+    self.log("This is a test trace message", type=LogLevel.trace, send_to_chat=True)
+    self.log("This is a test debug message", type=LogLevel.debug, send_to_chat=True)
+    self.log("This is a test info message", type=LogLevel.info, send_to_chat=True)
+    self.log("This is a test warning message", type=LogLevel.warn, send_to_chat=True)
+    self.log("This is a test error message", type=LogLevel.error, send_to_chat=True)
+    self.log("This is a test fatal message", type=LogLevel.fatal, send_to_chat=True)
+    self._print('')
