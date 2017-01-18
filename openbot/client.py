48e513e44d60da4f1985ec883788e33d31afb70b
John Hancock <jhancock50124+gh@gmail.com>
2017-01-18 09:24:10 -0500

    Removed testing code from client

--- openbot/client.py
+++ openbot/client.py
@@ -11,17 +11,6 @@ class BotClient(discord.Client):
     self.core.logger.log("{} [{}]".format(self.user.name, self.user.id), parent='core.info.bot_logged')
 
   async def on_message(self, message):
-    if message.content.startswith('!test'):
-      counter = 0
-      tmp = await self.send_message(message.channel, 'Calculating messages...')
-      async for log in self.lpogs_from(message.channel, limit=100):
-        if log.author == message.author:
-          counter += 1
-
-      await self.edit_message(tmp, 'You have {} messages.'.format(counter))
-    elif message.content.startswith('!sleep'):
-      await asyncio.sleep(5)
-      await self.send_message(message.channel, 'Done sleeping')
-
-    elif message.content.startswith('!stop'):
+    # Saved for later: self.core.config.get_config('core', 'command_prefix')
+    if message.content.startswith('!stop'):
       await self.logout()
