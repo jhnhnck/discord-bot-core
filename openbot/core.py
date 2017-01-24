c505b243a91c1f38839b89816de7b1b3d1620bde
John Hancock <jhancock50124+gh@gmail.com>
2017-01-24 10:28:27 -0500

    Changed loading order

--- openbot/core.py
+++ openbot/core.py
@@ -14,12 +14,22 @@ class BotCore:
   CORE_DESCRIPTION = "A simple, easily expandable, and well documented framework to add custom commands and \
                       interactions to Discord"
 
+  """
+  Init.
+
+  Loading Order.
+    1. Logger: In loaded in 'en_us' mode or provided locale mode
+    2. Config: The configuration is loaded from 'config/openbot.json' or the provided path
+    3. Permissions: The permissions are loaded from 'config/perms.json'
+    4. Plugins, Functions, Tasks: These are loaded into separate dictionaries via the Loader class
+  """
   def __init__(self, config_file, locale):
     self.logger = Logger(BotCore.CORE_VERSION, BotCore.CORE_RELEASE_TYPE, locale)
     self.logger.self_test()
 
     self.config = ConfigStream(self, self.logger, config_file=config_file)
-    self.server = BotClient(self, self.logger)
+
+    # TODO: Make permissions movable
     self.permissions = BotPerms()
 
     loader = Loader(self.logger, self.config)
@@ -29,17 +39,22 @@ class BotCore:
     self.functions = loader.load_functions(self.plugins)
     # self.tasks = loader.load_tasks()
 
+    # Provides each plugin with the
+    self._register_postinit()
+
+    self.server = BotClient(self, self.logger)
+
 
   def run(self):
     # Client token: '***REMOVED***'
     self.server.run(self.config.get_config("core.token"))
 
 
-  def _register_configs(self):
+  def _register_postinit(self):
       for name, plugin in self.plugins.items():
         # TODO: Log level=Debug "Config given to plugins [{}]" (array of registered plugins)
         pass
-        
+
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description=BotCore.CORE_DESCRIPTION)
