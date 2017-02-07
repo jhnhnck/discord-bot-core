0b5d04d766729c2b80b0a3f2bbb77315ecd89343
John Hancock <jhancock50124+gh@gmail.com>
2017-02-07 10:12:41 -0500

    Changed logger type parameter to autofill if possible

--- openbot/logger.py
+++ openbot/logger.py
@@ -13,16 +13,17 @@ class ParentScopeException(Exception):
 
 
 class LogLevel(Enum):
+  blank = -3
   trace = -2
   debug = -1
-  info = 0
-  warn = 1
-  error = 2
+  info = 1
+  warn = 2
+  error = 3
   fatal = 5
 
 
 locale = None
-
+log_types = ['trace', 'debug', 'info', 'warn', 'error', 'fatal']
 
 def setup(locale_name):
   global locale
@@ -42,34 +43,36 @@ def _load_locale(locale_name):
       exit(66)
 
 
-def log(message, parent='core.info.plaintext', type=LogLevel.info, error_point=None, send_to_chat=True):
+def log(message, parent='core.info.plaintext', log_type=None, error_point=None, send_to_chat=True):
+  log_type = _type_from_parent(parent, log_type)
+
   try:
     parent_string = get_locale_string(parent)\
       .format(message=message, error_point=error_point)
-    cli_base_string = get_locale_string("base.cli.{}_base".format(type.name))\
+    cli_base_string = get_locale_string("base.cli.{}_base".format(log_type.name))\
       .format(message=parent_string, **locale['colors'], **locale['format'])
     # TODO: Fix something here
 
     _print(cli_base_string)
 
     if send_to_chat:
-      chat_base_string = get_locale_string("base.chat.{}_base".format(type.name))\
+      chat_base_string = get_locale_string("base.chat.{}_base".format(log_type.name))\
         .format(message=parent_string)
       # TODO: Handle chat messages
       pass
 
   except ParentNotFoundException:
     # TODO: Fix this, don't to this ever -jhnhnck
-    log("\"{}\" with level \"{}\"".format(message, type.name),
+    log("\"{}\" with level \"{}\"".format(message, log_type),
         parent="core.error.locale_missing",
-        type=LogLevel.error,
+        log_type=LogLevel.error,
         error_point=parent,
         send_to_chat=send_to_chat)
   except ParentScopeException:
     # TODO: Fix this, don't to this ever -jhnhnck
-    log("\"{}\" with level \"{}\"".format(message, type.name),
+    log("\"{}\" with level \"{}\"".format(message, log_type),
         parent="core.error.locale_early_termination",
-        type=LogLevel.error,
+        log_type=LogLevel.error,
         error_point=parent,
         send_to_chat=send_to_chat)
   except IndexError:
@@ -96,12 +99,29 @@ def get_locale_string(parent):
 
   return store
 
+def _type_from_parent(parent, log_type):
+  if isinstance(log_type, LogLevel):
+    return log_type
+
+  elif log_type is None:
+    split = parent.split('.')
+    if split[1] in log_types:
+      return LogLevel[split[1]]
+    else:
+      for part in split[2:]:
+        if part in log_types:
+          return LogLevel[part]
+
+  else:
+    return LogLevel.info
+
 
 def self_test():
-  log("This is a test trace message", type=LogLevel.trace, send_to_chat=True)
-  log("This is a test debug message", type=LogLevel.debug, send_to_chat=True)
-  log("This is a test info message", type=LogLevel.info, send_to_chat=True)
-  log("This is a test warning message", type=LogLevel.warn, send_to_chat=True)
-  log("This is a test error message", type=LogLevel.error, send_to_chat=True)
-  log("This is a test fatal message", type=LogLevel.fatal, send_to_chat=True)
+  log("This is a test blank message; Don't use this", log_type=LogLevel.blank, send_to_chat=True)
+  log("This is a test trace message", log_type=LogLevel.trace, send_to_chat=True)
+  log("This is a test debug message", log_type=LogLevel.debug, send_to_chat=True)
+  log("This is a test info message", log_type=LogLevel.info, send_to_chat=True)
+  log("This is a test warning message", log_type=LogLevel.warn, send_to_chat=True)
+  log("This is a test error message", log_type=LogLevel.error, send_to_chat=True)
+  log("This is a test fatal message", log_type=LogLevel.fatal, send_to_chat=True)
   _print('')
