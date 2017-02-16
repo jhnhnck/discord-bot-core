737e735100d528a3e426d39778c8ef8a3c715898
John Hancock <jhancock50124+gh@gmail.com>
2017-02-16 10:05:35 -0500

    Added documentation for function structure

--- openbot/loader.py
+++ openbot/loader.py
@@ -28,7 +28,7 @@ def load_plugins():
   ever happen"^(tm).
 
   Plugin Structure.
-  Plugins are defined based upon a json file located in the root of the plugin directory (see Naming. above). You can
+  Plugins are defined based upon a json file located in the root of the plugin directory (see 'Naming.' above). You can
   make a copy of the 'coreftns.json' file within the 'resources/' directory commented example as a starting point. Note
   that this format and its fields may change with development without notice. The file may not always be up to date.
   As always the best example of plugin structure can be found in the 'jhnhnck_coreftns' plugin located at
@@ -92,7 +92,26 @@ def load_plugins():
 def load_functions(plugins):
   """
   Basic Function Structure.
-  TODO: Create a structure for functions to be loaded
+
+  Naming.
+  The function should be contained within a directory in the 'plugins/domain_plugin/functions/' directory. The file name
+  is not in a specific format but the recommended format is 'section_function.py'. This must be defined in the
+  'pluginname.json' file (See 'Function Structure.' below)
+
+  Function Structure.
+  Functions are defined based upon the 'functions' section in the json file located in the root of the plugin directory.
+  For more information on this file see 'Basic Plugin Structure. -> Plugin Structure.' above.
+
+  Additionally, the function must also have a python file that contains a class 'BotFunction' that inherits
+  'FunctionBase' from 'openbot.abstract.function'. This class must at least implement the 'call()' function and can
+  override the other methods present in the abstract class for more customization such as providing more advanced
+  loading requirements
+
+  Internal Function Structure.
+  Each plugin is loaded into a dictionary with all the keys for that function from the 'pluginname.json' file with three
+  additional keys for 'store' which contains the initialized class for that function, 'simple_string' which contains the
+  calling command in the form 'core.command_prefix + function_name', and 'qualified string' which contains the longer
+  calling command with the plugin prefix included in the form 'core.command_prefix + plugin_prefix + function_name'.
   """
   functions = {}
 
