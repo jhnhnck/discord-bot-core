6647938b091e39a8186b0cc2ae40705c8fd25b6f
John Hancock <jhancock50124+gh@gmail.com>
2017-03-28 10:19:07 -0400

    Moved self_test until after reloading of locale

--- openbot/core.py
+++ openbot/core.py
@@ -17,23 +17,22 @@ def startup(config_file, locale):
   """
   Startup.
   Loading Order.
-    1. Logger: In loaded in 'en_us' mode or provided locale mode first and reloaded if changed via config
+    1. Logger: In loaded in 'en_us' mode or provided locale mode
     2. Config: The configuration is loaded from 'config/openbot.json' or the provided path
       - Logger is reloaded if loaded in 'en_us' mode and config has a differing locale
-    3. Permissions: The permissions are loaded from 'config/perms.json' or the provided path
-    4. Plugins, Functions, Tasks: These are loaded into separate dictionaries via the Loader class
+    3. Plugins, Functions, Tasks: These are loaded into separate dictionaries via the Loader class
   """
   openbot.logger.setup(locale)
   _log_system_info()
-
-  if openbot.RELEASE_TYPE == 0:
-    openbot.logger.self_test()
 
   openbot.config.setup(config_file)
 
   # Logger is reloaded if loaded in 'en_us' mode and config has a differing locale
   if locale == 'en_us' and openbot.config.get_config('core.locale') != 'en_us':
     openbot.logger.setup(openbot.config.get_config('core.locale'))
+
+  if openbot.RELEASE_TYPE == 0:
+    openbot.logger.self_test()
 
   global plugins
   plugins = openbot.loader.load_plugins()
