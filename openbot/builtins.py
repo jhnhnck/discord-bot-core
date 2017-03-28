aa42fa1f3f53dc396ebe86f9e0d91a15c0613cc1
John Hancock <jhancock50124+gh@gmail.com>
2017-03-28 10:42:28 -0400

    Added file for builtin commands

---
+++ openbot/builtins.py
@@ -0,0 +1,13 @@
+"""
+Built-in Functions.
+This file will contain functions that are either too small or too vital to be included in the jhnhnck_coreftns plugin.
+These are fundamentally the same as any other function and could preform the same in a command, but is required to be
+loaded for the bot to work.
+
+Contents.
+  chat_bind, chat_reload, chat_restart, chat_shutdown, chat_sleep
+"""
+
+config = """\
+
+"""
