ea9c8ed20a07c05d99f5e0e8d743a2ef4ba46316
John Hancock <jhancock50124+gh@gmail.com>
2017-04-25 10:40:55 -0400

    Cleaned up git commit version enumerations

--- openbot/__init__.py
+++ openbot/__init__.py
@@ -7,6 +7,14 @@ class OpenbotRestart(Exception):
   pass
 
 
+def _latest_git_commit():
+  """Try to find latest git commit"""
+  try:
+    return subprocess.check_output(["git", "describe", "--always"]).decode()[:-1]
+  except:
+    return 'unknown'
+
+
 def _block_modules(*blocked):
   """
   Disable some of Discord.py's features from working 
@@ -26,15 +34,11 @@ RELEASE_TYPE_NAME = RELEASE_TYPES[RELEASE_TYPE]
 
 VERSION = '0.0.1'
 FULL_VERSION = '{}-{}'.format(VERSION, RELEASE_TYPE_NAME)
+GIT_VERSION = _latest_git_commit()
 
 DESCRIPTION = 'A simple, easily expandable, and well documented framework to add custom commands and \
                interactions to Discord'
 
-# Try to find latest git commit
-try:
-  GIT_VERSION = subprocess.check_output(["git", "describe", "--always"]).decode('utf-8')[:-1]
-except:
-  GIT_VERSION = 'unknown'
 """
 Reasons for blocking.
 discord.ext: This framework implements more structured and easy to understand 
@@ -44,3 +48,5 @@ _BLOCKED_MODULES = _block_modules('discord.ext')
 
 # Add plugin directory to path
 sys.path.insert(0, os.path.abspath('plugins'))
+
+
